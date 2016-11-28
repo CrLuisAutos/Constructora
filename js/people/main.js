1afd1d8a205fad1652d40e81644600ee38e3bff1
Luis\LuisPC <espinozaluis3680@gmail.com>
2016-11-27 19:36:14 -0600

    footer

--- js/people/main.js
+++ js/people/main.js
@@ -1,38 +1,67 @@
+var avatar;
+var id;
+var name;
+var people[];
+
+var Persister = {
+    save: function(key, value) {
+        localStorage.setItem(key, value);
+    },
+    load: function(key, default_value) {
+        return localStorage.getItem(key) || default_value;
+    },
+    saveObj: function(key, value) {
+        var json_string = JSON.stringify(value);
+        this.save(key, json_string);
+    },
+    loadObj: function(key, default_value) {
+        var json_string = this.load(key, default_value);
+        return JSON.parse(json_string);
+    }
+};
 jQuery(document).ready(function($) {
     /*evento del boton agregar persona*/
     $("#savePeople").click(function(ev) {
-        var avatar = $("#avatar").val();
-        var id = $("#id").val();
-        var name = $("#name").val();
-        if ((name !=="")&&(avatar!=="")&&(id!=="")) {
-        	cleanModal();
-        	$("inputSuccess").click();
+        avatar = $("#avatar").val();
+        id = $("#id").val();
+        name = $("#name").val();
+        if ((name !== "") && (avatar !== "") && (id !== "")) {
+            cleanModal();
+            $("inputSuccess").click();
+            people.push({
+                people: id,
+                avatar,
+                name
+            });
+            Persister.saveObj('people', people);
             return $(".table").append('<tr class="trPeople"><td class="table-striped">' + id + '</td><td class="table-striped">' + name + '</td><td class="table-striped">' + avatar + '</td><td class="table-striped"><a class="btnDelete"><span class="glyphicon glyphicon-trash" aria-hidden="true" ></span></a><a class="btnEdit"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a></td></tr>');
-        }
-        else{
+        } else {
             $("#msj").val("kgugujv");
         }
 
     });
-    /*Autofocus en el modal*/
-    $('#myModal3').on('shown.bs.modal', function() {
-    $("#id").focus();
-});
+    /*carga los datos del modal*/
+    function load_People() {
+
+    }
+
     /*Elimina row de la tabla*/
-    $('.table').on('click', '.btnDelete', function(event) {
+    $('.table').on('click', '.btnDelete', function() {
         event.preventDefault();
         $(this).parent().parent().remove();
-        
+
     });
-     /*Edita row de la tabla*/
-    $('.table').on('click', '.btnEdit', function(event) {
-        var valores[];
-        var i=0;
-         $(this).parents("tr").find("td").each(function(){
-                valores[i]=$(this).html();
-                i++;
-            });
-         alert(valores);
+    /*Edita row de la tabla*/
+    $('.table').on('click', '.btnEdit', function() {
+        event.preventDefault();
+
+        alert(ej);
+        cleanModal();
+    });
+
+    /*Autofocus en el modal*/
+    $('#myModal3').on('shown.bs.modal', function() {
+        $("#id").focus();
     });
 
     /*limpia los label del modal*/
@@ -40,6 +69,6 @@ jQuery(document).ready(function($) {
         $("#name").val("");
         $("#avatar").val("");
         $("#id").val("");
-        
+
     }
 });
