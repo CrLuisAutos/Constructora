de5553d770881507e5bd6c9a7a0084de29779fe7
Luis\LuisPC <espinozaluis3680@gmail.com>
2016-12-10 15:10:13 -0600

    task

--- js/project/details.js
+++ js/project/details.js
@@ -1,7 +1,38 @@
 var stateProject = [];
+var task= [];
 jQuery(document).ready(function($) {
 	$("#saveState").click(function(event) {
 		stateProject.color= $("#colorState").val();
-		alert(stateProject.color);
+		stateProject.name= $("#nameState").val();
+		createState();
 	});
+	$("saveTask").click(function(event) {
+		task.name= $("nameTask").val();
+		createTask();
+	});
+
 });
+//crea el estado del proyecto
+function createState () {
+	 $("#container").append('<div id="'+stateProject.name+'" class="cont-item" ondrop="drop(event)" ondragover="allowDrop(event)"></div>');
+	 document.getElementById(stateProject.name).style.backgroundColor = stateProject.color;
+}
+
+//crear tarea
+function createTask () {
+	$(".item").append('<div class="item" id="'+task.name+'" draggable="true" ondragstart="drag(event)">'+task.name+'</div>'); 
+}
+
+function allowDrop(ev) {
+    ev.preventDefault();
+}
+
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
